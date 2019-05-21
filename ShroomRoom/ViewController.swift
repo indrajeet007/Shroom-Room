f668db838446868fc1e5071f91b60eeac3f27b65
indrajeet007 <indrajeet.0072@gmail.com>
2019-05-21 18:19:12 +0530

    main driver, initial commit

--- ShroomRoom/ViewController.swift
+++ ShroomRoom/ViewController.swift
@@ -7,14 +7,194 @@
 //
 
 import UIKit
+import CircleMenu
+import CoreML
+import Vision
+import ImageIO
 
-class ViewController: UIViewController {
+extension UIColor {
+    static func color(_ red: Int, green: Int, blue: Int, alpha: Float) -> UIColor {
+        return UIColor(
+            red: 1.0 / 255.0 * CGFloat(red),
+            green: 1.0 / 255.0 * CGFloat(green),
+            blue: 1.0 / 255.0 * CGFloat(blue),
+            alpha: CGFloat(alpha))
+    }
+}
 
+class ViewController: UIViewController, CircleMenuDelegate {
+    
+//    @IBInspectable var buttonsCount: Int = 2
+//    @IBInspectable var duration: Double = 1 // circle animation duration
+//    @IBInspectable var distance: Float = 300 // distance between center button and buttons
+    
+    // MARK: - IBOutlets
+    
+//    @IBOutlet weak var imageView: UIImageView!
+//    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    var classificationLabel = UILabel()
+    
+    var classifiedLabel: String = ""
+    
+    let items: [(icon: String, color: UIColor)] = [
+        ("icon_home", UIColor(red: 0.19, green: 0.57, blue: 1, alpha: 1)),
+        ("icon_search", UIColor(red: 0.22, green: 0.74, blue: 0, alpha: 1))
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let button = CircleMenu(
+            frame: CGRect(x: 164, y: 348, width: 50, height: 50),
+            normalIcon:"icon_menu",
+            selectedIcon:"icon_close",
+            buttonsCount: 2,
+            duration: 1,
+            distance: 110)
+        button.delegate = self
+        button.layer.cornerRadius = button.frame.size.width / 2.0
+        view.addSubview(button)
     }
-
-
+        
+    // MARK: - Image Classification
+    
+    /// - Tag: MLModelSetup
+    lazy var classificationRequest: VNCoreMLRequest = {
+        do {
+            /*
+             Use the Swift class `MobileNet` Core ML generates from the model.
+             To use a different Core ML classifier model, add it to the project
+             and replace `MobileNet` with that model's generated Swift class.
+             */
+            let model = try VNCoreMLModel(for: MushroomDataset().model)
+            
+            let request = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
+                self?.processClassifications(for: request, error: error)
+            })
+            request.imageCropAndScaleOption = .centerCrop
+            return request
+        } catch {
+            fatalError("Failed to load Vision ML model: \(error)")
+        }
+    }()
+    
+    /// - Tag: PerformRequests
+    func updateClassifications(for image: UIImage) {
+        classificationLabel.text = "Classifying..."
+        
+        let orientation = CGImagePropertyOrientation(image.imageOrientation)
+        guard let ciImage = CIImage(image: image) else { fatalError("Unable to create \(CIImage.self) from \(image).") }
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            let handler = VNImageRequestHandler(ciImage: ciImage, orientation: orientation)
+            do {
+                try handler.perform([self.classificationRequest])
+            } catch {
+                /*
+                 This handler catches general image processing errors. The `classificationRequest`'s
+                 completion handler `processClassifications(_:error:)` catches errors specific
+                 to processing that request.
+                 */
+                print("Failed to perform classification.\n\(error.localizedDescription)")
+            }
+        }
+    }
+    
+    /// Updates the UI with the results of the classification.
+    /// - Tag: ProcessClassifications
+    func processClassifications(for request: VNRequest, error: Error?) {
+        DispatchQueue.main.async {
+            guard let results = request.results else {
+                self.classificationLabel.text = "Unable to classify image.\n\(error!.localizedDescription)"
+                return
+            }
+            // The `results` will always be `VNClassificationObservation`s, as specified by the Core ML model in this project.
+            let classifications = results as! [VNClassificationObservation]
+            
+            if classifications.isEmpty {
+                self.classificationLabel.text = "Nothing recognized."
+            } else {
+                // Display top classifications ranked by confidence in the UI.
+                let topClassifications = classifications.prefix(1) // 2 for two predictions
+                let descriptions = topClassifications.map { classification in
+                    // Formats the classification for display; e.g. "(0.37) cliff, drop, drop-off".
+                    // return String(format: "  (%.2f) %@", classification.confidence, classification.identifier)
+                    return String(format: "%@", classification.identifier)
+                }
+//                self.classificationLabel.text = "Classification:\n" + descriptions.joined(separator: "\n")
+                self.classifiedLabel = descriptions.joined()
+                
+                self.performSegue(withIdentifier: "ToExpandView", sender: self)
+            }
+        }
+    }
+    
+    // MARK: - Pass classify data to next ViewController
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "ToExpandView" {
+            if let destinationViewController = segue.destination as? ExpandViewController {
+                destinationViewController.classifyLabel = self.classifiedLabel
+            }
+        }
+    }
+    
+    func circleMenu(_: CircleMenu, willDisplay button: UIButton, atIndex: Int) {
+        button.backgroundColor = items[atIndex].color
+        
+        button.setImage(UIImage(named: items[atIndex].icon), for: .normal)
+        
+        button.layer.cornerRadius = button.frame.size.width / 2.0
+        
+        // set highlited image
+        let highlightedImage = UIImage(named: items[atIndex].icon)?.withRenderingMode(.alwaysTemplate)
+        button.setImage(highlightedImage, for: .highlighted)
+        button.tintColor = UIColor(red: 0, green: 0.34, blue: 0.50, alpha: 0.3)
+    }
+    
+    func circleMenu(_: CircleMenu, buttonWillSelected _: UIButton, atIndex: Int) {
+        print("button will selected: \(atIndex)")
+        
+    }
+    
+    func circleMenu(_: CircleMenu, buttonDidSelected _: UIButton, atIndex: Int) {
+        print("button did selected: \(atIndex)")
+        
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            presentPhotoPicker(sourceType: .photoLibrary)
+            return
+        }
+        
+        switch atIndex {
+        case 0:
+            presentPhotoPicker(sourceType: .camera)
+        case 1:
+            presentPhotoPicker(sourceType: .photoLibrary)
+        default:
+            print("hahaha")
+        }
+        
+    }
+    
+    func presentPhotoPicker(sourceType: UIImagePickerControllerSourceType) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = sourceType
+        present(picker, animated: true)
+    }
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    // MARK: - Handling Image Picker Selection
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
+        picker.dismiss(animated: true)
+        
+        // We always expect `imagePickerController(:didFinishPickingMediaWithInfo:)` to supply the original image.
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+//        imageView.image = image
+        updateClassifications(for: image)
+    }
+}
