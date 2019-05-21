17442815101027109743cf67c0921102d80f05b1
indrajeet007 <indrajeet.0072@gmail.com>
2019-05-21 18:17:30 +0530

    dependency files granted by the theme developer

--- ShroomRoom/Helpers/CollectionViewHelper.swift
+++ ShroomRoom/Helpers/CollectionViewHelper.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2019 Indrajit Chavan. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UICollectionView {
+    
+    func getReusableCellWithIdentifier<T: UICollectionViewCell>(_ indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withReuseIdentifier: T.cellIdentifier, for: indexPath) as? T else {
+            fatalError("Couldn't instantiate view controller with identifier \(T.cellIdentifier) ")
+        }
+        
+        return cell
+    }
+}
+
+// MARK: UITableViewCell
+protocol CollectionViewCellIdentifiable {
+    static var cellIdentifier: String { get }
+}
+
+extension CollectionViewCellIdentifiable where Self: UICollectionViewCell {
+    static var cellIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UICollectionViewCell: CollectionViewCellIdentifiable {}
